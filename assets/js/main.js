f84447a9bbc3f1d205a9e7d9d6c897070cf0d4e0
olgasolda <olyasis@gmail.com>
2023-03-07 19:42:36 +0200

    added sections about and form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,23 @@
-
-$(document).ready(function () {
-
-  $('.header-navbar__hamburger').on('click', function () {
-    $(this).toggleClass('active');
-    $('.header-navbar__list').toggleClass('active');
-  });
-
+// Плавное появление меню через всплывающую вкладку//
+$(document).ready(function(){
+	$('.header-navbar__btn').on('click', function () {
+		$(this).toggleClass('-active');
+    $('.header-navbar__list').stop(true, true).slideToggle(500);
+	});
 });
+
+//Плавное появление через прозрачнось//
+// $(document).ready(function(){
+// 	$('.header-navbar__btn').on('click', function () {
+// 		$(this).toggleClass('-active');
+//     $('.header-navbar__list').fadeToggle(500);
+// 	});
+// });
+
+//просто появление блока нажатием//
+// $(document).ready(function(){
+// 	$('.header-navbar__btn').on('click', function () {
+// 		$(this).toggleClass('-active');
+//     $('.header-navbar__list').toggleClass('-active');
+// 	});
+// });
